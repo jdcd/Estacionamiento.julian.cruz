6e6c7d4f6c8a557dd84cfede61728e557f22b6fb
Julian David Cruz Daza - Ceiba Software <julian.cruz@ceiba.com.co>
2019-05-16 07:18:16 -0500

    Para revisar deuda tecnica

--- src/main/java/co/com/ceiba/estacionamiento/EstacionamientoApplication.java
+++ src/main/java/co/com/ceiba/estacionamiento/EstacionamientoApplication.java
@@ -1,4 +1,4 @@
-package co.com.ceiba.estacionamiento.infrastructure;
+package co.com.ceiba.estacionamiento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
